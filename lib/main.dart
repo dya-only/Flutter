b6522f213f87b2de2089424df8893552f351985a
dya-only <dyacode@proton.me>
2023-04-06 20:25:28 +0900

    more uis

--- lib/main.dart
+++ lib/main.dart
@@ -70,19 +70,20 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 Container(
-                  margin: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                  margin: const EdgeInsets.fromLTRB(14, 10, 0, 20),
                   child: const Text(
                     '2023년 4월 6일 목요일',
                     style: TextStyle(
                       color: Colors.grey,
-                      fontSize: 20,
+                      fontSize: 18,
                     ),
                   ),
                 ),
                 Row(
                   children: <Widget>[
                     Container(
-                      margin: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                      height: 30,
+                      margin: const EdgeInsets.fromLTRB(0, 10, 10, 20),
                       child: OutlinedButton(
                         onPressed: () {
                           if (kDebugMode) {
@@ -97,11 +98,18 @@ class _MyHomePageState extends State<MyHomePage> {
                               const BorderSide(color: Colors.blue, width: 1.0),
                           shape: const StadiumBorder(),
                         ),
-                        child: const Text('오늘'),
+                        child: const Text(
+                          '오늘',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
                       ),
                     ),
                     Container(
-                      margin: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                      height: 30,
+                      margin: const EdgeInsets.fromLTRB(0, 10, 15, 20),
                       child: OutlinedButton(
                         onPressed: () {
                           if (kDebugMode) {
@@ -116,24 +124,16 @@ class _MyHomePageState extends State<MyHomePage> {
                               const BorderSide(color: Colors.grey, width: 1.0),
                           shape: const StadiumBorder(),
                         ),
-                        child: const Text('내일'),
+                        child: const Text(
+                          '내일',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
                       ),
                     ),
                   ],
-                ),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Container(
-                  margin: const EdgeInsets.fromLTRB(40, 40, 0, 20),
-                  child: const Text(
-                    '오늘의 일정',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30,
-                    ),
-                  ),
                 ),
               ],
             ),
@@ -143,65 +143,135 @@ class _MyHomePageState extends State<MyHomePage> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     Container(
-                      margin: const EdgeInsets.fromLTRB(3, 3, 3, 3),
-                      width: 180,
-                      height: 140,
+                      margin: const EdgeInsets.fromLTRB(5, 5, 5, 5),
+                      width: 190,
+                      height: 210,
                       child: Card(
                         shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16.0),
-                          side:
-                              const BorderSide(width: 1.0, color: Colors.blue),
+                          borderRadius: BorderRadius.circular(18.0),
+                          side: const BorderSide(
+                              width: 1.0,
+                              color: Color.fromARGB(255, 226, 226, 226)),
                         ),
                         elevation: 0.0,
                         color: Colors.white,
-                        child: Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: const <Widget>[
-                              Text(
-                                '6일(목)',
-                                style: TextStyle(
-                                  fontSize: 25,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.blue,
-                                ),
-                              ),
-                            ],
+                        child: Container(
+                          margin: const EdgeInsets.all(18),
+                          child: const Text(
+                            '시간표',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
                           ),
                         ),
                       ),
                     ),
                     Container(
-                      margin: const EdgeInsets.fromLTRB(3, 3, 3, 3),
-                      width: 180,
-                      height: 140,
+                      margin: const EdgeInsets.fromLTRB(5, 5, 5, 5),
+                      width: 190,
+                      height: 210,
                       child: Card(
                         shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16.0),
-                          side:
-                              const BorderSide(width: 1.0, color: Colors.blue),
+                          borderRadius: BorderRadius.circular(18.0),
+                          side: const BorderSide(
+                              width: 1.0,
+                              color: Color.fromARGB(255, 226, 226, 226)),
                         ),
                         elevation: 0.0,
                         color: Colors.white,
-                        child: Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: const <Widget>[
-                              Text(
-                                '7일(금)',
-                                style: TextStyle(
-                                  fontSize: 25,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.blue,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
+                        child: Container(
+                            margin: const EdgeInsets.all(18),
+                            child: const Text(
+                              '급식',
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            )),
                       ),
                     ),
                   ],
                 ),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Container(
+                    width: 390,
+                    height: 100,
+                    margin: const EdgeInsets.all(5),
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                        side: const BorderSide(
+                            width: 1.0,
+                            color: Color.fromARGB(255, 226, 226, 226)),
+                      ),
+                      child: Container(
+                        margin: const EdgeInsets.all(18),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Container(
+                                  margin: const EdgeInsets.fromLTRB(0, 0, 0, 4),
+                                  child: const Text(
+                                    '캘린더',
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                const Text(
+                                  '캘린더에 새로운 일정을 등록해보세요',
+                                  style: TextStyle(
+                                      fontSize: 15, color: Colors.grey),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    )),
+                Container(
+                    width: 390,
+                    height: 100,
+                    margin: const EdgeInsets.all(5),
+                    child: Card(
+                      color: const Color.fromARGB(255, 37, 126, 228),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                      ),
+                      child: Container(
+                        margin: const EdgeInsets.all(18),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Container(
+                                  margin: const EdgeInsets.fromLTRB(0, 0, 0, 4),
+                                  child: const Text(
+                                    '2023 나의 성적 그래프 만들기',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                const Text(
+                                  '#3월모의고사 #성적그래프',
+                                  style: TextStyle(
+                                      fontSize: 15, color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    )),
               ],
             ),
             Container(
